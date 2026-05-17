49882ea075cb62067c065e6ab310c5416772fe72
felipebriones26 <fel.briones@duocuc.cl>
2026-05-17 17:49:25 -0400

    fix: mapear propiedad entregado a la columna fisica despachado para solucionar error 500

--- src/main/java/com/citt/persistence/entity/Despacho.java
+++ src/main/java/com/citt/persistence/entity/Despacho.java
@@ -1,5 +1,5 @@
 package com.citt.persistence.entity;
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -28,5 +28,6 @@ public class Despacho {
     //@NotBlank(message = "La dirección es obligatoria")
     private String direccionCompra;
     private Long valorCompra;
+    @Column(name = "despachado")
     private boolean entregado = false;
 }
