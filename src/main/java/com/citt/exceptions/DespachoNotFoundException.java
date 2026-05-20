052d12a4832769916a616459bdd1c08e965d42a9
Alexis-Aranguiz <alex.aranguiz@duocuc.cl>
2026-05-19 20:03:16 -0400

    Cambios pa probar

--- src/main/java/com/citt/exceptions/DespachoNotFoundException.java
+++ src/main/java/com/citt/exceptions/DespachoNotFoundException.java
@@ -3,5 +3,6 @@ package com.citt.exceptions;
 public class DespachoNotFoundException extends Exception{
     public DespachoNotFoundException(String message) {
         super(message);
+        
     }
 }
